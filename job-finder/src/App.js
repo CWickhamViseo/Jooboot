a7c08054181dc10963f3eb73b76e7ee6a1e3ea6d
Wickham <CWI3613@viseo.net>
2017-09-14 10:41:10 +0200

    basis

---
+++ job-finder/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            jsonResponse: "",
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    JSON response be here
+                    {this.state.jsonResponse}
+                </p>
+            </div>
+        );
+    }
+
+    componentDidMount(){
+        
+    }
+}
+
+export default App;
